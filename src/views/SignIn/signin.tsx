5bcb0dfe107c539d8141e91a90f9f43b5a08927e
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-18 20:32:43 +0200

    feat: create SignIn view

---
+++ src/views/SignIn/signin.tsx
@@ -0,0 +1,9 @@
+import CenterWrapper from 'components/atoms/CenterWrapper/centerWrapper';
+
+const SignIn = (): JSX.Element => (
+  <CenterWrapper>
+    <div>SignIn</div>
+  </CenterWrapper>
+);
+
+export default SignIn;
