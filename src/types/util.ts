ef2a4694c31f3e5d8d145c91a9494dabea341e29
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-21 12:04:13 +0200

    feat: fetching document from database

--- src/types/util.ts
+++ src/types/util.ts
@@ -2,3 +2,13 @@ import { Editor } from 'slate';
 
 export type MarkTypes = 'bold' | 'italic' | 'code' | 'underline' | 'sup';
 export type ToggleMarkFun = (editor: Editor, format: MarkTypes) => void;
+
+export interface IDocument {
+  _id: string;
+  name: string;
+  content: string;
+  access: string;
+  owner: string;
+  permissions: string;
+  users: string[];
+}
--- src/views/Documents/documents.tsx
+++ src/views/Documents/documents.tsx
@@ -1,17 +1,47 @@
 import { IconButton } from '@material-ui/core';
 import { useAuthContext } from 'context/auth/AuthContext';
 import LogoutIcon from '@material-ui/icons/ExitToAppOutlined';
+import { useFetchContext } from 'context/fetch/FetchContext';
+import { useEffect, useState } from 'react';
+import { IDocument } from 'types/util';
+import { useSnackbar } from 'notistack';
+import styles from './document.module.scss';
 
 const Documents = (): JSX.Element => {
   const { logout } = useAuthContext();
+  const { authAxios } = useFetchContext();
+  const [documents, setDocuments] = useState<IDocument[] | null>(null);
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    const getUserDocuments = async (): Promise<void> => {
+      try {
+        const response = await authAxios?.get('/document/get');
+        if (response?.data !== undefined)
+          setDocuments(response.data as IDocument[]);
+      } catch (e) {
+        enqueueSnackbar('Unknown error occured.', { variant: 'error' });
+      }
+    };
+
+    getUserDocuments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-      <IconButton onClick={() => logout()}>
-        <LogoutIcon />
-      </IconButton>
-      <div>Documents</div>
-    </>
+    <div className={styles.wrapper}>
+      <div>
+        <IconButton onClick={() => logout()}>
+          <LogoutIcon />
+        </IconButton>
+        <div>Documents</div>
+        <div>
+          {documents?.map((doc) => (
+            <p key={doc._id}>{doc.name}</p>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
