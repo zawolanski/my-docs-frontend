d0081513e15a11961c6e18d85041b479f2ef5c4f
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-21 11:36:30 +0200

    feat: cheking that user is logged in

--- src/components/atoms/AuthRoute/authRoute.tsx
+++ src/components/atoms/AuthRoute/authRoute.tsx
@@ -1,13 +1,43 @@
-import { useAuthContext } from 'context/auth/AuthContext';
+/* eslint-disable no-nested-ternary */
+import { CircularProgress } from '@material-ui/core';
+import { useFetchContext } from 'context/fetch/FetchContext';
+import { useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import CenterWrapper from 'components/atoms/CenterWrapper/centerWrapper';
 import { IAuthRouteProps } from './types';
 
 const AuthRoute = ({ children, ...props }: IAuthRouteProps): JSX.Element => {
-  const { isAuthenticated } = useAuthContext();
+  const { authAxios } = useFetchContext();
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkUserLogged = async (): Promise<void> => {
+      try {
+        await authAxios?.get('/user/data');
+        setIsAuth(true);
+        setIsLoading(false);
+      } catch (e) {
+        setIsAuth(false);
+        setIsLoading(false);
+      }
+    };
+
+    checkUserLogged();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Route {...props}>
-      {isAuthenticated() ? children : <Redirect to="/" />}
+      {isLoading ? (
+        <CenterWrapper>
+          <CircularProgress />
+        </CenterWrapper>
+      ) : isAuth ? (
+        children
+      ) : (
+        <Redirect to="/" />
+      )}
     </Route>
   );
 };
