cd6a07d5efd4102a07522a049cfb5efe89d8de9e
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-21 20:11:03 +0200

    feat: create document component

---
+++ src/components/molecules/Document/documents.tsx
@@ -0,0 +1,25 @@
+import {
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Avatar,
+} from '@material-ui/core';
+import DescriptionIcon from '@material-ui/icons/Description';
+import { Link } from 'react-router-dom';
+import { IDocumentProps } from './types';
+import styles from './document.module.scss';
+
+const Document = ({ name }: IDocumentProps): JSX.Element => (
+  <Link to="/">
+    <ListItem button>
+      <ListItemIcon>
+        <Avatar>
+          <DescriptionIcon />
+        </Avatar>
+      </ListItemIcon>
+      <ListItemText className={styles.text} primary={name} />
+    </ListItem>
+  </Link>
+);
+
+export default Document;
