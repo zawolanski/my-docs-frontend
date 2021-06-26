7710327f6307afe6ae3f528160b7e5aaf2af7f68
Sebastian <sebastian.zawolanski@gmail.com>
2021-06-26 18:17:03 +0200

    feat: change theme to dark

--- src/templates/Main/main.tsx
+++ src/templates/Main/main.tsx
@@ -1,17 +1,21 @@
+import { ThemeProvider } from '@material-ui/core';
 import { AuthProvider } from 'context/auth/AuthContext';
 import { FetchProvider } from 'context/fetch/FetchContext';
 import SnackbarTemplate from 'templates/Snackbar/snackbar';
+import { MUItheme } from 'theme/mui';
 
 const MainTemplate = ({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element => (
-  <SnackbarTemplate>
-    <AuthProvider>
-      <FetchProvider>{children}</FetchProvider>
-    </AuthProvider>
-  </SnackbarTemplate>
+  <ThemeProvider theme={MUItheme}>
+    <SnackbarTemplate>
+      <AuthProvider>
+        <FetchProvider>{children}</FetchProvider>
+      </AuthProvider>
+    </SnackbarTemplate>
+  </ThemeProvider>
 );
 
 export default MainTemplate;
