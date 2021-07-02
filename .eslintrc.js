92309e6e10934e8ad386b56e30a08ba176b529b5
Sebastian <sebastian.zawolanski@gmail.com>
2021-07-02 14:47:48 +0200

    chore: disable @typescript-eslint/no-non-null-assertion

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,6 +49,7 @@ module.exports = {
     'no-use-before-define': 'off',
     'react/jsx-props-no-spreading': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'import/extensions': [
       'error',
