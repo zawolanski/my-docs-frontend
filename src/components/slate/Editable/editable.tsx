ed61c8ba347928ed2af7d50bd77923b63b13c4ab
Sebastian <sebastian.zawolanski@gmail.com>
2021-07-12 19:29:42 +0200

    feat: switching script button when opposite is selected

--- src/components/slate/Editable/editable.tsx
+++ src/components/slate/Editable/editable.tsx
@@ -3,7 +3,6 @@ import { useCallback } from 'react';
 import { Editable as Edit, RenderLeafProps, useSlate } from 'slate-react';
 import { toggleMark } from 'util/mark';
 import { Leaf } from 'components/slate/Elements/Leaf';
-import { useAuthContext } from 'context/auth/AuthContext';
 import { HOTKEYS } from 'util/hotkeys';
 import isHotkey from 'is-hotkey';
 import { Keys, MarkTypes } from 'types/util';
@@ -11,8 +10,6 @@ import styled from './editable.module.scss';
 import { EditableProps } from './types';
 
 const Editable = ({ permissions }: EditableProps): JSX.Element => {
-  const { data: authData } = useAuthContext();
-
   const renderLeaf = useCallback(
     (props: RenderLeafProps) => <Leaf {...props} />,
     []
@@ -22,9 +19,6 @@ const Editable = ({ permissions }: EditableProps): JSX.Element => {
 
   return (
     <div className={styled.wrapper}>
-      {authData.email}
-      {authData.firstname}
-      <br />
       <div className={styled['editor-wrapper']}>
         <Edit
           readOnly={!(permissions === 'editor')}
--- src/util/mark.ts
+++ src/util/mark.ts
@@ -1,3 +1,4 @@
+import { remove } from 'lodash';
 import { Editor } from 'slate';
 import { MarkTypes } from '../types/util';
 
@@ -6,8 +7,28 @@ export const isMarkActive = (editor: Editor, format: MarkTypes): boolean => {
   return marks ? marks[format] === true : false;
 };
 
+export const isMarkShouldToggle = (
+  editor: Editor,
+  elements: MarkTypes[]
+): boolean => {
+  const marks = Editor.marks(editor);
+  if (marks !== null)
+    Object.keys(marks).forEach((mark) => {
+      elements.forEach((el) => {
+        if (el === mark) Editor.removeMark(editor, el);
+      });
+    });
+  return true;
+};
+
 export const toggleMark = (editor: Editor, format: MarkTypes): void => {
   const isActive = isMarkActive(editor, format);
+
+  if (format === 'sub' || format === 'sup')
+    isMarkShouldToggle(
+      editor,
+      remove(['sub', 'sup'], (n) => n !== format)
+    );
 
   if (!isActive) Editor.addMark(editor, format, true);
   else Editor.removeMark(editor, format);
