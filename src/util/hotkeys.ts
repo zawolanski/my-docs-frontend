14bb3fb8d89441f0c028efbbcf22062e82c1c187
Sebastian <sebastian.zawolanski@gmail.com>
2021-07-12 18:55:19 +0200

    feat: add hotkeys

---
+++ src/util/hotkeys.ts
@@ -0,0 +1,10 @@
+export const HOTKEYS = {
+  'mod+b': 'bold',
+  'mod+i': 'italic',
+  'mod+u': 'underline',
+  'mod+`': 'code',
+  'mod+.': 'sup',
+  'mod+,': 'sub',
+  'mod+5': 'strikethrough',
+  'mod+k': 'link',
+};
