33c7b7e7ed1307202f84fddac6673ceda8c6f300
Sebastian <sebastian.zawolanski@gmail.com>
2021-07-15 19:57:59 +0200

    feat: add HeadingElement

--- src/types/slate.d.ts
+++ src/types/slate.d.ts
@@ -1,10 +1,16 @@
 import { BaseEditor } from 'slate';
 import { ReactEditor } from 'slate-react';
+import { HeadingVariants } from './util';
 
 export type ParagraphElement = { type: 'paragraph'; children: CustomText[] };
 export type CodeElement = { type: 'code'; children: CustomText[] };
 export type LinkElement = { type: 'link'; url: string; children: Descendant[] };
 export type BlockQuoteElement = { type: 'block-quote'; children: Descendant[] };
+export type HeadingElement = {
+  type: 'heading';
+  variant: HeadingVariants;
+  children: Descendant[];
+};
 
 export type CustomText = {
   text: string;
